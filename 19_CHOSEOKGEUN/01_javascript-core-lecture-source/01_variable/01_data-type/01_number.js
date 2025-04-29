733d0b6a3196ed48bbdae16ee7264cb3bcc2fb21
seokgeun_cho <choseokgeun@gmail.com>
2025-04-29 17:46:17 +0900

    커밋

---
+++ 19_CHOSEOKGEUN/01_javascript-core-lecture-source/01_variable/01_data-type/01_number.js
@@ -0,0 +1,11 @@
+// 한줄 주석
+/* 여러줄 주석 */
+
+/* 숫자 타입 */
+var interger = 10;
+var double = 5.5;
+var negative = -10;
+console.log(interger);
+console.log(double);
+console.log(negative);
+console.log(interger == 10.0);
